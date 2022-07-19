65c29f96e3157e8e759b31f0d3d9cd2d1886bdcd
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2022-07-19 16:37:11 -0400

    Click outside closes menu

--- components/header.js
+++ components/header.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef } from "react";
 import Link from "next/link";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,12 +6,13 @@ import { faBars, faXmark, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 import Container from "./UI/container";
+import { useOnClickOutside } from "../hooks";
 
 import styles from "./header.module.scss";
 
-export default function Header({}) {
-  const [menu, setMenu] = useState(false);
-
+export default function Header({ menu, setMenu }) {
+  const closeMenu = () => setMenu(false);
+  const toggleMenu = () => setMenu(!menu);
   const navStyle = {
     closed: `${styles.nav}`,
     open: `${styles.nav} ${styles["nav--open"]}`,
@@ -21,20 +22,20 @@ export default function Header({}) {
     open: `${styles.menu} ${styles["menu--open"]}`,
   };
 
+  const node = useRef();
+  useOnClickOutside(node, () => setMenu(false));
+
   return (
     <header className={styles.header}>
       <Container>
-        <nav className={menu ? navStyle.open : navStyle.closed}>
+        <nav ref={node} className={menu ? navStyle.open : navStyle.closed}>
           <div className={styles.main}>
             <Link href="/">
-              <a className={styles.main__home}>
+              <a onClick={closeMenu} className={styles.main__home}>
                 <h1>Hiroto Kaku</h1>
               </a>
             </Link>
-            <button
-              className={styles.main__button}
-              onClick={() => setMenu(!menu)}
-            >
+            <button className={styles.main__button} onClick={toggleMenu}>
               <FontAwesomeIcon icon={menu ? faXmark : faBars} />
             </button>
           </div>
@@ -42,32 +43,44 @@ export default function Header({}) {
           <ul className={menu ? menuStyle.open : menuStyle.closed}>
             <li className={styles.menu__item}>
               <Link href="/about-me">
-                <a>About Me</a>
+                <a onClick={closeMenu}>About Me</a>
               </Link>
             </li>
 
-            <li className={styles.menu__item}>
+            {/* <li className={styles.menu__item}>
               <Link href="/blog">
-                <a>Blog</a>
+                <a onClick={closeMenu} >Blog</a>
               </Link>
-            </li>
+            </li> */}
 
             <hr className={styles.menu__item}></hr>
 
             <li className={styles.menu__item}>
               <ul className={styles.menu__socials}>
                 <li className={styles.socials__item}>
-                  <a href="https://github.com/thekakkun/">
+                  <a
+                    href="https://github.com/thekakkun/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
                     <FontAwesomeIcon icon={faGithub} />
                   </a>
                 </li>
                 <li className={styles.socials__item}>
-                  <a href="https://www.linkedin.com/in/hirotokaku/">
+                  <a
+                    href="https://www.linkedin.com/in/hirotokaku/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
                     <FontAwesomeIcon icon={faLinkedin} />
                   </a>
                 </li>
                 <li className={styles.socials__item}>
-                  <a href="mailto:kaku.hiroto@gmail.com">
+                  <a
+                    href="mailto:kaku.hiroto@gmail.com"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
                     <FontAwesomeIcon icon={faEnvelope} />
                   </a>
                 </li>

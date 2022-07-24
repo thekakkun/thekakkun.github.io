8d4761a13b167a45c1365499332250757cf96811
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2022-07-24 13:28:53 -0400

    Add home button

--- components/ui/layout.js
+++ components/ui/layout.js
@@ -1,21 +1,33 @@
 import { useState } from "react";
 
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 import Footer from "../footer";
 import Container from "./container";
 import Header from "../header";
 
-import styles from './layout.module.scss'
-
+import styles from "./layout.module.scss";
 
 export default function Layout({ children }) {
   const [menu, setMenu] = useState(false);
+  const currentRoute = useRouter().pathname;
 
   return (
     <div className={styles.body}>
       <Header menu={menu} setMenu={setMenu} />
 
       <main className={styles.main}>
-        <Container>{children} </Container>
+        <Container>
+          {children}
+          {currentRoute != "/" ? (
+            <div className={styles.backToHome}>
+              <Link href="/">
+                <a>← Back to home</a>
+              </Link>
+            </div>
+          ) : null}
+        </Container>
       </main>
 
       <Footer />

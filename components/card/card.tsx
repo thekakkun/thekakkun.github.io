4822198eb0cbbdd7bcbadbecbfbf25f0588d1777
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2022-10-31 11:54:16 -0400

    Remove attempt at masonry (not great from accessibility standpoint)

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
 
 import { imageLoader } from "../../loader";
 import styles from "./card.module.scss";
@@ -19,45 +18,9 @@ export default function Card({
   subtitle,
   title,
   info,
-  cols,
 }: CardProps) {
-  const [top, setTop] = useState<number>(0);
-  const cardRef = useRef<HTMLAnchorElement>(null);
-
-  // useEffect(() => {
-  //   function handleResize() {
-  //     if (!cardRef.current) {
-  //       throw Error("cardRef is not assigned.");
-  //     } else if (cols) {
-  //       let cardAbove: HTMLElement = cardRef.current;
-
-  //       for (let i = 0; i < cols; i++) {
-  //         if (cardAbove.previousElementSibling) {
-  //           cardAbove = cardAbove.previousElementSibling as HTMLElement;
-  //         } else {
-  //           setTop(0);
-  //           return;
-  //         }
-  //       }
-  //       console.log(cardRef.current, cardAbove);
-
-  //       const gapBetween =
-  //         cardRef.current.offsetTop -
-  //         (cardAbove.offsetTop + cardAbove.offsetHeight);
-  //       setTop(-gapBetween);
-  //     }
-  //   }
-
-  //   addEventListener("resize", handleResize);
-  //   handleResize();
-
-  //   return () => {
-  //     removeEventListener("resize", handleResize);
-  //   };
-  // }, [cols]);
-
   return (
-    <a className={styles.card} href={href} ref={cardRef} style={{ top: top }}>
+    <a className={styles.card} href={href}>
       <p className={styles.title}>{title}</p>
       <p className={styles.subtitle}>{subtitle}</p>
       <Image
